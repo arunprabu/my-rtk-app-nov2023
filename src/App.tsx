4b7a121aa9b0df09ac4b09783f19fefad0517842
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-11-28 18:37:27 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
+import { BrowserRouter } from "react-router-dom"
 import "./App.css"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <h1>Success!</h1>
+      </main>
+
+      <footer className="text-center">
+        <hr />
+        <p>Copyright 2023 | Arun</p>
+      </footer>
+    </BrowserRouter>
   )
 }
 
